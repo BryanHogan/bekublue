487bdc47ac49b05c6045141f8f1b13077b1459c6
BryanHogan <bryanhoganme@gmail.com>
2024-11-08 20:25:04 +0100

    building index

--- src/content/config.ts
+++ src/content/config.ts
@@ -16,13 +16,26 @@ const reviewCollection = defineCollection({
         coverAlt: z.string(),
         pubDate: z.date(),
         tags: z.array(z.string()),
+        rating: z.number().max(5, "Max rating is 5.").min(0, "Minimum rating is 0"),
     })
 });
 
 const postCollection = defineCollection({
-    type: "content",
+    type: 'content',
     schema: ({ image }) => z.object({
-        title: z.string().max(60, "Maximum of 60 characters in title required.").min(10, "Minimum of 10 characters in title required."),
+        title: z.string().max(75, "Maximum of 75 characters in title required.").min(1, "Minimum of 1 characters in title required."),
+        description: z.string().max(200, "Maximum of 200 characters in description required.").min(25, "Minimum of 25 characters in description required."),
+        author: z.string(),
+        cover: image()
+            .refine((img) => img.width = 1600, {
+                message: "Blog hero image must be 1600 pixel wide!",
+            })
+            .refine((img) => img.height = 900, {
+                message: "Blog hero image must be 900 pixel high!",
+            }),
+        coverAlt: z.string(),
+        pubDate: z.date(),
+        tags: z.array(z.string()),
     })
 });
 
