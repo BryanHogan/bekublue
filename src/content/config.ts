7063053a9b2e8942a007857100966a25fd321fa3
BryanHogan <bryanhoganme@gmail.com>
2024-12-17 23:15:07 +0100

    blog post bottom information and alt frontmatter removed

--- src/content/config.ts
+++ src/content/config.ts
@@ -5,7 +5,7 @@ const reviewCollection = defineCollection({
     schema: ({ image }) => z.object({
         title: z.string().max(75, "Maximum of 75 characters in title required.").min(1, "Minimum of 1 characters in title required."),
         description: z.string().max(200, "Maximum of 200 characters in description required.").min(25, "Minimum of 25 characters in description required."),
-        author: z.string(),
+        author: z.string().optional(),
         cover: image()
             .refine((img) => img.width > 350, {
                 message: "Cover image must be at least 350 pixel wide!",
@@ -13,7 +13,6 @@ const reviewCollection = defineCollection({
             .refine((img) => img.height > 200, {
                 message: "Cover image must be at least 200 pixel high!",
             }),
-        coverAlt: z.string(),
         hero: image()
             .refine((img) => img.width > 350, {
                 message: "Hero image must be at least 350 pixel wide!",
@@ -21,7 +20,6 @@ const reviewCollection = defineCollection({
             .refine((img) => img.height > 200, {
                 message: "Hero image must be at least 200 pixel high!",
             }),
-        heroAlt: z.string(),
         pubDate: z.date(),
         tags: z.array(z.string()),
         rating: z.number().max(5, "Max rating is 5.").min(0, "Minimum rating is 0"),
